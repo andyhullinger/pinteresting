fac09b3a37f81f16d4a8c8a3c50eefd59a3b98ad
Andy Hullinger <andyhullinger@mac.com>
2014-06-14 16:15:55 -0500

    added imageupload with paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# specifically set the paperclip path
+Paperclip.options[:command_path] = '/usr/local/bin'
+
